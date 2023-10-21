8f4433266a16c6ca982c17cb198cad855ae662cd
Eetu Remes <eetu.remes1997@gmail.com>
2023-10-21 18:22:41 +0300

    + dict

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
