10b479aa9711bd6d3f8f3161c8211f062870a618
Eetu Remes <eetu.remes1997@gmail.com>
2023-10-22 02:47:43 +0300

    uibuild

---
+++ models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('Yhdistetty MongoDB')
+    })
+    .catch((error) => {
+        console.log('Virhe yhdistäessä tietokantaan:', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
